e7bdd7b303583772056ca5f4309a36d830c40e77
Jean-X <fernandesjeanluc@gmail.com>
2016-04-20 14:52:18 +0200

    anciens

--- src/fr/umlv/graph/Graph.java
+++ src/fr/umlv/graph/Graph.java
@@ -44,9 +44,9 @@ public class Graph {
 
   public static Graph toGraph(int itr[][]){
 	  Graph graph = new Graph((itr.length * itr[0].length) + 2);
-	  for(int i = 0; i<itr.length; i++){
+	  for(int i = 0; i < itr.length; i++){
 		  graph.addEdge(new Edge(itr.length * itr[0].length, itr[0].length*i, 1000, 0));
-		  for(int j = 0; j<itr[0].length; j++){
+		  for(int j = 0; j < itr[0].length; j++){
 			  if(j+1 < itr[0].length)
 				  graph.addEdge(new Edge(itr[0].length*i + j, itr[0].length*i + j + 1, itr[i][j], 0));
 			  if(j == itr[0].length - 1)
@@ -56,10 +56,18 @@ public class Graph {
 			  if(j-1 >= 0 && i-1 >= 0)
 				  graph.addEdge(new Edge(itr[0].length*i + j, itr[0].length*(i-1) + j - 1, 1000, 0));
 			  if(j-1 >= 0 && i+1 < itr.length)
-				  graph.addEdge(new Edge(itr[0].length*i + j, itr[0].length*(i+1) + j-1, 1000, 0));
+				  graph.addEdge(new Edge(itr[0].length*i + j, itr[0].length*(i+1) + j - 1, 1000, 0));
 		  }
 	  }
 	  return graph;
+  }
+  
+  public void initGraph(){
+	 Iterable<Edge> iterable = edges();
+	 for(Edge e : iterable){
+			 System.out.println(e.from +" "+ e.to + " " + e.capacity);
+			 
+	 }
   }
   
   public void writeFile(Path path) throws IOException {
--- src/fr/umlv/graph/Main.java
+++ src/fr/umlv/graph/Main.java
@@ -10,6 +10,7 @@ public class Main {
 		int image[][] = SeamCarving.readpgm(Paths.get("test.pgm"));
 		int interest[][] = SeamCarving.interest(image);
 		Graph graph = Graph.toGraph(interest);
+		graph.initGraph();
 		graph.writeFile(FileSystems.getDefault().getPath(".", "access.log"));
 		SeamCarving.writepgm(image, "result.pgm");
 	}
